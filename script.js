2b81e18c622ae23f1b92946d56d6f6795687d466
AllanGd <allan_gd@hotmail.com>
2022-07-05 13:18:57 -0300

    update

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-function colorirTela(){
+function coletar(){
   let cor = document.getElementById('colorPicker').value;
-  document.getElementById('corpo').style.backgroundColor= cor;
+  
+  let valor = document.querySelector('input[name="local"]:checked').value;
+  if (valor!==null){
+    
+    switch (valor){
+      case "titulo":
+        document.getElementById('cabecalho').style.color= cor;
+        break;
+      case "fundo":
+        document.getElementById('corpo').style.backgroundColor= cor;
+        break;
+      default:
+      break;
+    }
+  }
+}
+function limpar(){
+  document.getElementById('cabecalho').style.color= "black";
+  document.getElementById('corpo').style.backgroundColor= "white";
+  document.getElementById('colorPicker').value = "#000"
 }
